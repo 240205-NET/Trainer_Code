0130277a42852e4b2b3f9f197543ff246aeea270
Richard Hawkins <Richard.Hawkins@revature.com>
2024-02-19 16:21:05 -0500

    add W3 and HttpConsumer

---
+++ W3/HTTPConsumer/HTTPConsumer.DTO/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace HTTPConsumer.DTO
+{
+    public class Post
+    {
+        // Fields
+        public int id { get; set; }
+        public int userId { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        // Constructors
+
+        // Methods
+    }
+}
---
+++ W3/HTTPConsumer/HTTPConsumer.app/Program.cs
@@ -0,0 +1,60 @@
+﻿using HTTPConsumer.DTO;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace HTTPConsumer.App
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("HTTP Consumer Console App Starting...");
+
+            // create an HTTP Client object
+            HttpClient client = new HttpClient();
+
+            // retrieve ALL of the posts
+            // prepare the info you need to make your request...
+            int? postNumber = 45;
+            string uri = $"http://jsonplaceholder.typicode.com/posts/{postNumber}";
+
+            // HTTP Status Codes
+            // 1xx's - Info 
+            // 2xx's - Good response
+            // 3xx's - Redirection
+            // 4xx's - Client error
+            // 404 - resource not found
+            // 403 - access denied/forbidden
+            // 418 - I am a teapot
+            // 5xx's - Server error
+            
+
+            // Async and await
+            // Async(ronous) - run this thing, but you CAN do other things while you're waiting for this...
+            // Await - this is the thing you're going to wait for a result from.
+            string HTTPresponse = await client.GetStringAsync(uri);
+
+            // When we use 'async' and 'await' the containing method must be asynchronous
+            // any async method MUST return a Task (an object that denotes if the method has completed or not)
+            // in addition to any object or type that the method would otherwise return
+
+            // Console.WriteLine(HTTPresponse);
+
+            //List<Post> posts = JsonSerializer.Deserialize<List<Post>>(HTTPresponse);
+
+            Post posts = JsonSerializer.Deserialize<Post>(HTTPresponse);
+
+            Console.WriteLine($"{posts.userId}: {posts.title}\n{posts.body}\n\n");
+
+            //Console.WriteLine($"{posts[45].userId}: {posts[45].title}\n{posts[45].body}\n\n");
+
+            //foreach (var item in posts)
+            //{
+            //    Console.WriteLine($"{item.userId}: {item.title}\n{item.body}\n\n");
+            //}
+        }
+    } // close the Program class
+
+
+}
