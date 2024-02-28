29602206572a223eb4a89275356abf7de308e776
jnipr <minseon.song@revature.com>
2024-02-28 15:06:13 -0500

    js fetch post

--- W4/TheGardenFrontEnd/addplant.js
+++ W4/TheGardenFrontEnd/addplant.js
@@ -4,11 +4,17 @@ async function addNewPlant(e) {
     const fd = new FormData(form)
 
     let data = {};
-    for(let [k, v] of fd.entries()) {
+    for(let [k, v]of fd.entries()) {
         console.log(k, v)
-        data[k] = v;
+        if(k === 'zone' || k === 'size') {
+            data[k] = parseInt(v)
+        }
+        else {
+            data[k] = v;
+        }
     }
-
+    console.log(data)
+    console.log('display loading...')
     const response = await fetch("http://localhost:5239/api/Plant", {
         method: "POST",
         body: JSON.stringify(data),
@@ -16,5 +22,7 @@ async function addNewPlant(e) {
             "Content-Type": "application/json"
         }
     })
-    console.log(response.json())
+    form.reset()
+    alert('plant successfully added!')
+    console.log('stuff happened...')
 }
