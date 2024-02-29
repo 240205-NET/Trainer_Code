9d77e51d32bb0ac9001b1d3d674e0365b1d411ab
jnipr <minseon.song@revature.com>
2024-02-29 15:54:02 -0500

    ts demo complete

--- W4/TheGardenFETypeScript/src/addplant.ts
+++ W4/TheGardenFETypeScript/src/addplant.ts
@@ -1,3 +1,36 @@
-async function addNewPlant() : Promise<void> {
-    
+import { Plant } from './models'
+
+async function addNewPlant(e : Event) : Promise<void> {
+    e.preventDefault()
+
+    const form : HTMLFormElement = <HTMLFormElement> document.getElementsByName('addPlantForm')[0]
+
+    const fd : FormData = new FormData(form)
+
+    let data : Plant = {
+        commonName: '',
+        scientificName: '',
+        size: 0,
+        zone: 0,
+        adoptionDate: ''
+    }
+    for(let kvp of fd.entries()) {
+        const key : keyof Plant = kvp[0] as keyof Plant
+        if(key === 'zone' || key === 'size') {
+            data[key] = parseInt(kvp[1].toString())
+        }
+        else if(key === 'commonName' || key === 'scientificName' || key === 'adoptionDate') {
+            data[key] = kvp[1].toString();
+        }
+    }
+
+    const response : Response = await fetch("http://localhost:5239/api/Plant", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    form.reset()
+    alert('plant successfully added!')
 }
--- W4/TheGardenFETypeScript/src/output/addplant.js
+++ W4/TheGardenFETypeScript/src/output/addplant.js
@@ -1,1 +1,43 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function addNewPlant(e) {
+    return __awaiter(this, void 0, void 0, function* () {
+        e.preventDefault();
+        const form = document.getElementsByName('addPlantForm')[0];
+        const fd = new FormData(form);
+        let data = {
+            commonName: '',
+            scientificName: '',
+            size: 0,
+            zone: 0,
+            adoptionDate: ''
+        };
+        for (let kvp of fd.entries()) {
+            const key = kvp[0];
+            if (key === 'zone' || key === 'size') {
+                data[key] = parseInt(kvp[1].toString());
+            }
+            else if (key === 'commonName' || key === 'scientificName' || key === 'adoptionDate') {
+                data[key] = kvp[1].toString();
+            }
+        }
+        const response = yield fetch("http://localhost:5239/api/Plant", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        form.reset();
+        alert('plant successfully added!');
+    });
+}
